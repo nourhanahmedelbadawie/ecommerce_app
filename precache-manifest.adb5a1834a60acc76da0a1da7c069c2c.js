4e4867bd1560122eaae8df2be3b9bbbb610d43ab
Nourhan Ahmed <nourhanahmedelbadawie@gmail.com>
2020-11-01 14:07:12 +0200

    Updates

---
+++ precache-manifest.adb5a1834a60acc76da0a1da7c069c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939c42f88635737d348c7d7f909b160b",
+    "url": "/ecommerce_app/index.html"
+  },
+  {
+    "revision": "2ba9d90296e8405c67bc",
+    "url": "/ecommerce_app/static/css/main.07eac36c.chunk.css"
+  },
+  {
+    "revision": "55b35907ff692b1c6ebc",
+    "url": "/ecommerce_app/static/js/2.f1b29a01.chunk.js"
+  },
+  {
+    "revision": "17bd1449cf0c020b1f5fd79c67ea7639",
+    "url": "/ecommerce_app/static/js/2.f1b29a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba9d90296e8405c67bc",
+    "url": "/ecommerce_app/static/js/main.4502154d.chunk.js"
+  },
+  {
+    "revision": "aa22733816e6afe5fa0a",
+    "url": "/ecommerce_app/static/js/runtime-main.b0a39d1d.js"
+  },
+  {
+    "revision": "a53de6fcb9c0468dcff870e35cbaa94f",
+    "url": "/ecommerce_app/static/media/bg.a53de6fc.jpg"
+  }
+]);
